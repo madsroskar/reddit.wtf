677839b43a830f2befd5232ad18f595ef2648ec7
Mads Røskar <madshvero@gmail.com>
2019-02-21 17:34:15 +0100

    Updates

---
+++ precache-manifest.8dccc5a6e3d196b263319aef96f69690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reddit.wtf/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f01a0286f3275b3bf20",
+    "url": "/reddit.wtf/static/js/runtime~main.9f01a028.js"
+  },
+  {
+    "revision": "4ec0a4026d57717efa67",
+    "url": "/reddit.wtf/static/js/main.4ec0a402.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/reddit.wtf/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "4ec0a4026d57717efa67",
+    "url": "/reddit.wtf/static/css/main.d724c244.chunk.css"
+  },
+  {
+    "revision": "6a15b0f6a530b4285dcb2f1d9ffe3222",
+    "url": "/reddit.wtf/index.html"
+  }
+];
